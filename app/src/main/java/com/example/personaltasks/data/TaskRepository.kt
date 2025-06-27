8fa1292d8c49cff112432bc2c56be035d41f83a8
caio-soares13 <caio.vitsoares@gmail.com>
2025-06-27 01:36:17 -0300

    feat: adicionada persistência remota pelo Firestore

--- app/src/main/java/com/example/personaltasks/data/TaskRepository.kt
+++ app/src/main/java/com/example/personaltasks/data/TaskRepository.kt
@@ -1,16 +1,32 @@
 package com.example.personaltasks.data
 
 import com.example.personaltasks.model.Task
+import com.google.firebase.firestore.FirebaseFirestore
 
 class TaskRepository(private val taskDao: TaskDao) {
+    private val firestore = FirebaseFirestore.getInstance()
+    private val collection = firestore.collection("tasks")
 
-    suspend fun getAllActiveTasks(): List<Task> = taskDao.getAllActiveTasks()
+    suspend fun getAllActiveTasks(): List<Task> {
+        return taskDao.getAllActiveTasks()
+    }
 
-    suspend fun getAllDeletedTasks(): List<Task> = taskDao.getAllDeletedTasks()
+    suspend fun getAllDeletedTasks(): List<Task> {
+        return taskDao.getAllDeletedTasks()
+    }
 
-    suspend fun insertTask(task: Task): Long = taskDao.insertTask(task)
+    suspend fun insertTask(task: Task) {
+        taskDao.insertTask(task)
+        collection.document(task.id.toString()).set(task)
+    }
 
-    suspend fun updateTask(task: Task) = taskDao.updateTask(task)
+    suspend fun updateTask(task: Task) {
+        taskDao.updateTask(task)
+        collection.document(task.id.toString()).set(task)
+    }
 
-    suspend fun deleteTask(task: Task) = taskDao.deleteTask(task)
+    suspend fun deleteTask(task: Task) {
+        taskDao.deleteTask(task)
+        collection.document(task.id.toString()).delete()
+    }
 }
